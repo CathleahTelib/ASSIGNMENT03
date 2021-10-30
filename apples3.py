611de8e0494e00ca86a985f9781859c63b4fdb80
Cathleah Tabaquero <92615682+CathleahTelib@users.noreply.github.com>
2021-10-30 15:58:06 +0700

    Update apples3.py

--- apples3.py
+++ apples3.py
@@ -1,9 +1,9 @@
 def ask_quantity_of_apple():
-    applesQuantity = int(input("How many apples you want to buy?: "))
+    applesQuantity = int(input("Apples you want to buy:"))
     return applesQuantity
 
 def ask_quantity_of_orange():
-    orangeQuantity =int(input("How many oranges you want to buy?: "))
+    orangeQuantity =int(input("Oranges you want to buy:"))
     return orangeQuantity
 
 def compute_total_amount():
@@ -16,7 +16,6 @@ def compute_total_amount():
     return amount
 
 #steps
-
 # 1. ask for apples you want to buy and save to variable
 applesQ = ask_quantity_of_apple()
 # 2. ask for oranges you want to buy and save to variable
@@ -26,5 +25,5 @@ print("The price of apples is 20 php")
 apples = 20
 print("The price of oranges is 25 php")
 oranges = 25
-# 4. display the total amount in the ff. format : The total amount is ________.
+# 4. display the total amount in the ff. format : The total amount is ___.
 amount = compute_total_amount()
